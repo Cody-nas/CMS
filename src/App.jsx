c5660a1ccb98e13182e3bb837a09d17a752fa4fc
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-07-02 18:19:13 +0100

    Implement initial project structure and setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-fuchsia-800 underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
